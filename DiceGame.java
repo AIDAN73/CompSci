4d9516db29d6a7575d66aae08155bed31e916d0d
Aidan Murphree <Murph.Aida2019@ssusd.org>
2019-01-16 07:07:36 -0800

    Finished change to dual-integers

--- DiceGame.java
+++ DiceGame.java
@@ -1,32 +1,32 @@
 class DiceGame {
 
-    static int D4;
-    static int D6;
-    static int D20;
+    static int D4roll;
+    static int D6roll;
+    static int D20roll;
 
 
 public static void main(String[] args) {
 
-        int D4 = getD4();
-        int D6 = getD6();
-        int D20 = getD20();
+        D4roll = getD4();
+        D6roll = getD6();
+        D20roll = getD20();
         reportNumbers();
 
     }
 
-    public static void getD4() {
+    public static int getD4() {
 
         int D4 = (int) (Math.random()*4)+1;
         return D4;
     }
 
-    public static void getD6() {
+    public static int getD6() {
 
         int D6 = (int) (Math.random()*6)+1;
         return D6;
     }
 
-    public static void getD20() {
+    public static int getD20() {
 
         int D20 = (int) (Math.random()*20)+1;
         return D20;
@@ -34,7 +34,7 @@ public static void main(String[] args) {
 
     public static void reportNumbers() {
         
-        System.out.println("\tD4:"+D4 +"\tD6:"+D6+"\tD20:"+D20);
+        System.out.println("\tD4:"+D4roll +"\tD6:"+D6roll+"\tD20:"+D20roll);
     }
 
 
