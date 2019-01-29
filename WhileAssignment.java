351d5e4435be4f11533bc001a9b00c31f1971383
Aidan Murphree <Murph.Aida2019@ssusd.org>
2019-01-29 06:50:18 -0800

    1st Checkpoint

---
+++ WhileAssignment.java
@@ -0,0 +1,12 @@
+class WhileAssignment
+{
+    
+
+    public static void main(String[] args) 
+    {
+        for(int i = 0; i < 1000; i += i) 
+        {
+            System.out.println(i);
+        }
+    }
+}
